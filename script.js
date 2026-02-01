271ffefcd83729b13fb8a812be33cfb29939b9a3
Isra Khlifi <khlifiisra736@gmail.com>
2026-02-01 22:45:56 +0100

    Updated portfolio

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("show");
 });
 
+(function () {
+  emailjs.init("-yagqRfY4q1sA-qd8");
+})();
+
+const form = document.getElementById("contact-form");
+const formStatus = document.getElementById("form-status");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  formStatus.textContent = "Sending message...";
+
+  emailjs.sendForm(
+    "service_0acm0lt",
+    "template_llh0srk",
+    this
+  )
+  .then(() => {
+    formStatus.textContent = "✅ Message sent successfully!";
+    form.reset();
+  })
+  .catch((error) => {
+    formStatus.textContent = "❌ Failed to send message.";
+    console.error(error);
+  });
+});
